67f593feca33e72912b38d8c2f1c686f99c5d03b
NodokaAoki <genta91nya@gmail.com>
2020-03-07 16:26:33 +0000

    [Finish]devise(member,admin)

---
+++ app/models/member.rb
@@ -0,0 +1,8 @@
+class Member < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  enum status: {有効: 0, 退会: 1,停止: 2}
+end
---
+++ test/controllers/members_controller_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class MembersControllerTest < ActionDispatch::IntegrationTest
+  test "should get show" do
+    get members_show_url
+    assert_response :success
+  end
+
+  test "should get delete_me" do
+    get members_delete_me_url
+    assert_response :success
+  end
+
+  test "should get withdrow" do
+    get members_withdrow_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get members_edit_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get members_update_url
+    assert_response :success
+  end
+
+end
