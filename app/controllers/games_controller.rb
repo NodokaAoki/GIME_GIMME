fa297729127c4e4b1cecc791bcd2f656b24e8a24
NodokaAoki <genta91nya@gmail.com>
2020-03-08 11:54:51 +0000

    playtime追加

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,6 +2,7 @@ class GamesController < ApplicationController
   def new
     @game = Game.new
     @playtime = @game.playtimes.build
+    @genres = Genre.where(status: 'true')
   end
 
   def create
@@ -12,6 +13,17 @@ class GamesController < ApplicationController
   end
 
   def show
+    @game = Game.find(params[:id])
+    @playtimes = @game.playtimes
+    @playtime = Playtime.new
+  end
+
+  def playtime_create
+    @playtime = Playtime.new(playtime_params)
+    @playtime.game_id = params[:id]
+    @playtime.member_id = current_member.id
+    @playtime.save!
+    redirect_to game_path(@playtime.game_id)
   end
 
   def edit
@@ -28,6 +40,9 @@ class GamesController < ApplicationController
 
   private
   def game_params
-    params.require(:game).permit(:title,:release,:number_of_people,:text,:complete,:orbit,:multi_ending,:site_url,:status,playtimes_attributes: [:id, :time,:member_id])
+    params.require(:game).permit(:title,:release,:number_of_people,:text,:complete,:orbit,:multi_ending,:site_url,:status,:genre_id,:cero_rating,playtimes_attributes: [ :time,:member_id])
+  end
+  def playtime_params
+    params.require(:playtime).permit(:time,:member_id)
   end
 end
--- db/migrate/20200308065749_add_member_id_to_playtimes.rb
+++ db/migrate/20200308065749_add_member_id_to_playtimes.rb
@@ -1,5 +1,5 @@
 class AddMemberIdToPlaytimes < ActiveRecord::Migration[5.2]
   def change
-    add_column :playtimes, :member_id, :integer,nill: false
+    add_column :playtimes, :member_id, :integer
   end
 end
