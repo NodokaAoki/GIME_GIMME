c300b9552549f25b0f795f3a9e3f07bbf75f38da
NodokaAoki <genta91nya@gmail.com>
2020-03-08 07:05:41 +0000

    playtime追加

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,14 @@
 class GamesController < ApplicationController
   def new
     @game = Game.new
+    @playtime = @game.playtimes.build
   end
 
   def create
+    @game = Game.new(game_params)
+    @game.member_id = current_member.id
+    @game.save!
+    redirect_to game_path(@game.id)
   end
 
   def show
@@ -19,5 +24,10 @@ class GamesController < ApplicationController
   end
 
   def hidden
+  end
+
+  private
+  def game_params
+    params.require(:game).permit(:title,:release,:number_of_people,:text,:complete,:orbit,:multi_ending,:site_url,:status,playtimes_attributes: [:id, :time,:member_id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_08_012534) do
+ActiveRecord::Schema.define(version: 2020_03_08_065749) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -63,6 +63,14 @@ ActiveRecord::Schema.define(version: 2020_03_08_012534) do
     t.index ["email"], name: "index_members_on_email", unique: true
     t.index ["name"], name: "index_members_on_name", unique: true
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
+  end
+
+  create_table "playtimes", force: :cascade do |t|
+    t.integer "game_id", null: false
+    t.integer "time", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "member_id"
   end
 
 end
