10924a03191d57bf65ec425ded67c508f7f3b6ac
NodokaAoki <genta91nya@gmail.com>
2020-03-29 17:02:58 +0900

    admin

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
   devise_for :admins, controllers: {
       sessions:      'admins/sessions',
       passwords:     'members/passwords'
+      registrations: 'members/registrations'
   }
   devise_for :members, controllers: {
   		sessions:      'members/sessions',
